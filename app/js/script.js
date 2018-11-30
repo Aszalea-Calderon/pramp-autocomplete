d293100732bd943c9d2d283d8de4d2a9631cc91a
Eunice Park <28845173+eunicode@users.noreply.github.com>
2018-11-30 15:03:41 -0800

    Add event listener, event handler, generate list function

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,31 @@
-console.log(`Hi, JS is working`);
+// Check this file is loaded from HTML
+// console.log(`Hi, JS is working`);
+
+// Select DOM node
+const inputElm = document.querySelector('.search-input');
+console.log({ inputElm });
+
+// Add event listener
+inputElm.addEventListener('input', passString);
+
+// Event handler
+function passString(event) {
+    // Get input string
+    let inputStr = event.target.value;
+    console.log({ inputStr });
+
+    // Make API call, get data, filter data with input string
+    const data = searchData(inputStr);
+    // Generate list items with filtered data array 
+    generateListItems(data);
+}
+
+function generateListItems(data) {
+    const ul = document.querySelector('.results');
+
+    data.map( elm => {
+        const li = document.createElement('li');
+        li.innerHTML = elm.title;
+        ul.appendChild(li);
+    });
+}
