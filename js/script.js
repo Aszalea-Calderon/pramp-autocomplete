ffeafdcc07f68cbc0892fe50eb14528034cb1638
Eunice Park <28845173+eunicode@users.noreply.github.com>
2018-11-30 15:13:56 -0800

    Updates

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// Check this file is loaded from HTML
+// console.log(`Hi, JS is working`);
+
+// Select DOM node
+const inputElm = document.querySelector('.search-input');
+console.log({ inputElm });
+
+// Add event listener
+inputElm.addEventListener('input', passString);
+
+// Event handler
+function passString(event) {
+    // Get input string
+    let inputStr = event.target.value;
+    console.log({ inputStr });
+
+    // Make API call, get data, filter data with input string
+    // searchData returns a promise that resolves to a filtered array
+    searchData(inputStr)
+         // Generate list items with filtered data array 
+        .then( (data) => generateListItems(data));
+}
+
+function generateListItems(data) {
+    // Select parent node
+    const ul = document.querySelector('.results');
+
+    // Clear old autocomplete suggestions
+    ul.innerHTML = '';
+
+    // Generate list elements for every item in array
+    data.map( elm => {
+        const li = document.createElement('li');
+        li.innerHTML = elm.title;
+        ul.appendChild(li);
+    });
+}
